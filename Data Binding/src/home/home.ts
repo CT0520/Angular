c0ca12c1be8feaf55880170b9ef3a70469337ab1
ajay from fedora <ajayprofessional119@gmail.com>
2026-03-13 13:10:57 +0530

    Databinding

---
+++ Data Binding/src/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  imports: [FormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  name = 'Ajay'
+  division = "6A6"
+  url = 'https://media.istockphoto.com/id/1550071750/photo/green-tea-tree-leaves-camellia-sinensis-in-organic-farm-sunlight-fresh-young-tender-bud.jpg?s=612x612&w=0&k=20&c=RC_xD5DY5qPH_hpqeOY1g1pM6bJgGJSssWYjVIvvoLw='
+
+  clickMe() {
+    alert("user Alert")
+  }
+}
